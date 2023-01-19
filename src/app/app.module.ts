7ecba377a994a03c16e06e3bf1891daea033f41e
Cristian Gavilan <cristiangavilan@gmail.com>
2023-01-19 11:34:30 -0300

    Se agrega prettierrc para formatear codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -10,25 +10,18 @@ import { HeaderModule } from './shared/header/header.module';
 import { BlocksModule } from './blocks/blocks.module';
 import { PagesModule } from './pages/pages.module';
 
-
-
 @NgModule({
-  declarations: [
-    AppComponent,  
-    
-  ],
+  declarations: [AppComponent],
   imports: [
-      BrowserModule, 
-      HttpClientModule,
-      AppRoutingModule,      
-     SharedModule,
-     HeaderModule,
-     BlocksModule,
-     PagesModule
-     
-     
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
+    HeaderModule,
+    BlocksModule,
+    PagesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
