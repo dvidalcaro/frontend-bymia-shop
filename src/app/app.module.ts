b741d12e31bfbccc23d2ee37d9ae6191ee740147
Cristian Gavilan <cristiangavilan@gmail.com>
2023-01-29 18:12:41 -0300

    Add new navbar to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,28 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './shared/header/header.module';
 import { BlocksModule } from './blocks/blocks.module';
 import { PagesModule } from './pages/pages.module';
 import { UserPanelModule } from './user-panel/user-panel.module';
 import { InterceptorsModule } from './interceptors/interceptors.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BymiaService } from './services/bymia.service';
-import { HeaderModule } from './shared/header/header.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    SharedModule,    
+    SharedModule,
     BlocksModule,
     PagesModule,
     UserPanelModule,
-    HeaderModule
+    HeaderModule,
+    FontAwesomeModule,
   ],
   exports: [],
   providers: [BymiaService],
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { navLink } from '../shared/interfaces/navlink';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  urlLogo: string = '../assets/img/logo_bymia.png';
+  WishNotifications: number = 1;
+  CartNotifications: number = 2;
+  switchBar: boolean = false;
+  showListPrice: boolean = false;
+  showConfirm: boolean = false;
+
+  linksHeader: navLink[] = [
+    {
+      label: 'Nosotros',
+      links: 'nosotros',
+    },
+
+    {
+      label: 'FAQs',
+      links: 'faqs',
+    },
+
+    {
+      label: 'Ofertas',
+      links: 'ofertas',
+    },
+
+    {
+      label: 'Lanzamientos',
+      links: 'lanzamientos',
+    },
+
+    {
+      label: 'Flash Sales',
+      links: 'flash-sales',
+    },
+    {
+      label: 'Precio De Lista',
+      reference: 'listPrice',
+    },
+    {
+      icon: 'bx bx-tada-hover bx-sm bx-search',
+      reference: 'search',
+    },
+    {
+      icon: 'bx bx-tada-hover bx-sm bxs-heart',
+      links: 'wish-list',
+    },
+    {
+      icon: 'bx bx-tada-hover bx-sm bxs-cart',
+      links: 'shop-cart',
+    },
+    {
+      icon: 'bx bx-tada-hover bx-sm bxs-user-circle',
+      links: 'login',
+    },
+  ];
+  constructor() {}
+
+  toogleBarSearch() {
+    this.switchBar = !this.switchBar;
+  }
+  toogleListPrice() {
+    this.showListPrice = !this.showListPrice;
+  }
+  openConfirm() {
+    this.showConfirm = true;
+    this.showListPrice = false;
+  }
+
+  closeConfirm() {
+    this.showConfirm = false;
+  }
+
+  ngOnInit(): void {}
+}
