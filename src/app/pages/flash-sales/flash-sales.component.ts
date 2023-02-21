431ff960bf1a37217024f2670b7f497b33035c13
Cristian Gavilan <cristiangavilan@gmail.com>
2023-02-21 12:12:32 -0300

    add content to sections

--- src/app/pages/flash-sales/flash-sales.component.ts
+++ src/app/pages/flash-sales/flash-sales.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { BymiaService } from 'src/app/services/bymia.service';
+import { FeaturedProduct } from 'src/app/shared/interfaces/FeaturedProduct';
 
 @Component({
   selector: 'app-flash-sales',
   templateUrl: './flash-sales.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class FlashSalesComponent implements OnInit {
+  flashSalesProducts: FeaturedProduct[] = [];
+  index: number = 0;
+  limit: number = 4;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private bymiaService: BymiaService) {
+    bymiaService
+      .getFeaturedProducts('destacados', this.index, this.limit)
+      .subscribe(resp => {
+        this.flashSalesProducts = resp;
+      });
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { BymiaService } from 'src/app/services/bymia.service';
+import { FeaturedProduct } from 'src/app/shared/interfaces/FeaturedProduct';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class OffersComponent implements OnInit {
+  bestOfferProducts: FeaturedProduct[] = [];
+  index: number = 0;
+  limit: number = 4;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private bymiaService: BymiaService) {
+    bymiaService
+      .getFeaturedProducts('destacados', this.index, this.limit)
+      .subscribe(resp => {
+        this.bestOfferProducts = resp;
+      });
   }
 
+  ngOnInit(): void {}
 }
