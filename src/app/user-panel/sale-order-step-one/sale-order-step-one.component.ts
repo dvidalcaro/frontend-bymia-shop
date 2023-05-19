e75820a377f5b513f9ad70764b145381daa464eb
Cristian Gavilan <cristiangavilan@gmail.com>
2023-05-19 00:14:43 -0300

    End order hardcoding to test

--- src/app/user-panel/sale-order-step-one/sale-order-step-one.component.ts
+++ src/app/user-panel/sale-order-step-one/sale-order-step-one.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/user/services/user.service';
 
 @Component({
   selector: 'app-sale-order-step-one',
   templateUrl: './sale-order-step-one.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SaleOrderStepOneComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  endOrder() {
+    this.userService.endOrder().subscribe(res => {
+      console.log('resp:', res);
+    });
   }
 
+  ngOnInit(): void {}
 }
