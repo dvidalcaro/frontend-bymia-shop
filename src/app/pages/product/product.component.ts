be556fc5ffa65fc4a9da923a543bf2dde1d6484f
Diego Vidal Caro <dvidalcaro@hotmail.com>
2023-06-02 02:42:15 -0300

    add img prodcuto y descripcion, add mas vendidos

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { BymiaService } from 'src/app/services/bymia.service';
+import { HomeSection } from 'src/app/shared/interfaces/HomeSection';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  homeSections: HomeSection[] = [];
+  constructor(public bymiaService: BymiaService) {
+    bymiaService.getSections().subscribe(resp => {
+      this.homeSections = resp;
+    });
   }
 
+  ngOnInit(): void {}
 }
